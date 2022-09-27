1efd968badd303ec76152d7433bdf3dd8c63184d
イオリ <i.takahashi.sys22@morijyobi.ac.jp>
2022-09-27 10:32:15 +0900

    プログラム編集

--- pra2.java
+++ pra2.java
@@ -1,3 +1,7 @@
-for(int i=0;i<100;i++){
-	System.out.println("Hello Git World");
+public class pra2.java
+public static void main(String[] args){
+	for(int i=0;i<100;i++){
+		System.out.println("Hello Git World");
+	}
 }
+
